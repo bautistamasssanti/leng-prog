c63aa9673327f236c70dfaf486999014edce788e
bautista massanti <bautista.massanti@ing.austral.edu.ar>
2024-08-06 00:46:08 -0300

    practice 1 done

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,12 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz a
+  | a <= 0 = Nothing
+  | otherwise = Just(collatzHelper a 0)
+
+collatzHelper :: Integer -> Integer -> Integer
+collatzHelper value counter
+  | value == 1 = counter
+  | value `mod` 2 == 0 = collatzHelper (value `div` 2) (counter + 1)
+  | otherwise = collatzHelper (value * 3 + 1) (counter + 1)
--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -5,27 +5,60 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add (n1, d1) (n2, d2) =
+  let
+    numerator = n1 * d2 + n2 * d1
+    divisor = d1 * d2
+    comDiv = hcf numerator divisor
+    numHcf = numerator `div` comDiv
+    divHcf = divisor `div` comDiv
+  in (numHcf, divHcf)
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (n1, d1) (n2, d2) =
+  let
+    numerator = n1 * d2 - n2 * d1
+    divisor = d1 * d2
+    comDiv = hcf numerator divisor
+    numHcf = numerator `div` comDiv
+    divHcf = divisor `div` comDiv
+  in (numHcf, divHcf)
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (n1, d1) (n2, d2) =
+  let
+    numerator = n1 * n2
+    divisor = d1 * d2
+    comDiv = hcf numerator divisor
+    numHcf = numerator `div` comDiv
+    divHcf = divisor `div` comDiv
+  in (numHcf, divHcf)
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (n1, d1) (n2, d2) =
+  let
+    numerator = n1 * d2
+    divisor = n2 * d1
+    comDiv = hcf numerator divisor
+    numHcf = numerator `div` comDiv
+    divHcf = divisor `div` comDiv
+  in (numHcf, divHcf)
+  
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
+hcf 0 b = b
+hcf a 0 = a
+hcf a b = hcf b remainder
+  where remainder = a `mod` b
+
 
 
     
